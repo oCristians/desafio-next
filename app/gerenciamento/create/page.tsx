61caa8c363071c4352f5ad40dc3df82d8261ed4f
oCristian <cristianbassis1@gmail.com>
2024-09-09 23:05:13 -0300

    front-Crud

--- app/gerenciamento/create/page.tsx
+++ app/gerenciamento/create/page.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link'
 export default function Page() {
     return(
-        <div className="z-40 absolute flex justify-center items-center w-full h-full bg-[#46432f]">
-            <form autoComplete="off" className="w-full lg:w-4/12 xl:w-7/12 2xl:w-5/12 gap-3 flex flex-col p-4 justify-center items-center rounded-xl bg-[#643D2A] ">
+        <div>
+            <Link href="../" className='z-0 absolute w-full h-full bg-[#46432f]'></Link>
+            <form autoComplete="off" className="w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute
+            lg:w-4/12 xl:w-7/12 2xl:w-5/12 gap-3 flex flex-col p-4 justify-center items-center rounded-xl bg-[#643D2A] ">
                 <div className="w-full flex justify-end text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl 3xl:text-7xl text-[#E9DFCC]">
-                    <i className="bi bi-x"></i>
+                    <Link href="../"><i className="bi bi-x"></i></Link>
                 </div>
                 <div className="text-xl font-bold text-[#E9DFCC] md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl 3xl:text-6xl">Crie Seu Produto</div>
                 <div className="flex flex-col w-full justify-center gap-2 rounded-xl p-2 bg-[#6C4633] ">
@@ -29,11 +32,11 @@ export default function Page() {
                 <div className="flex gap-2">
                 <button
                 type="submit"
-                className="bg-green-600 text-3xl font-bold text-[#352630] py-2 px-4 rounded-xl">
+                className="bg-green-600 text-xl lg:text-3xl font-bold text-[#352630] py-2 px-4 rounded-xl">
                 Criar
                 </button>
                 <button
-                className="bg-[#8E7E7E] text-3xl font-bold text-[#352630] py-2 px-4 rounded-xl">
+                className="bg-[#8E7E7E] text-xl lg:text-3xl font-bold text-[#352630] py-2 px-4 rounded-xl">
                 Cancelar
                 </button>
                 </div>
